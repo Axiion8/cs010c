8420d80821c8f4cbe01bdf4912ddcf31bf298825
anj501 <110501128+anj501@users.noreply.github.com>
2022-11-17 14:29:32 -0800

    Add files via upload

---
+++ Lab_7/Node.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+    public:
+        string data;
+        int count;
+        Node* left;
+        Node* right;
+        Node* parent;
+        Node(string d);
+};
+
+#endif
